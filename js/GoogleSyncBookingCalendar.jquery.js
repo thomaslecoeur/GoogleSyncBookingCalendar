330f6c6046fc2595c0860706c67ae6f2efec93d5
Thomeuxe <thomeuxe@gmail.com>
2015-02-16 14:22:51 +0100

    Correction of a bug of booking restriction

---
+++ js/GoogleSyncBookingCalendar.jquery.js
@@ -0,0 +1,110 @@
+jQuery(document).ready(function($) {
+	var startSelected = false;
+	var endSelected = false;
+
+	/* Init the calendar */
+
+	$('#calendar').datepicker({
+		inline: true,
+		numberOfMonths: 12,
+		showOtherMonths: true,
+		minDate: 0,
+		maxDate: "+11m",
+		hideIfNoPrevNext: true,
+		onSelect: function (date, inst) {
+        	inst.inline = false; // Disable calendar reset
+        	if(!startSelected && !endSelected){
+        		$('#start-date').val(date);
+        	}else if(startSelected && !endSelected){
+        		$('#end-date').val(date);
+        	}
+        	else{
+        		$('#start-date').val("");
+        		$('#end-date').val("");
+        	}
+        }
+    });
+
+	/* Set the language of the calendar */
+
+	$.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );
+
+	/* Adding navigation buttons */
+
+	$('#calendar').append('<nav><button class="nav prev">Previous</button><button class="nav next">Next</button></nav>');
+
+
+
+
+	$('#calendar td').click(function(event) {
+		if(!startSelected && !endSelected){
+			if(!$(this).is('.booked:not(.end-booking)')){
+				startSelected = true;
+				$(this).addClass('selected-booking');
+				$(this).prepend('<aside class="bookingInfo-start">Début du séjour</aside>');
+			}else{
+				$(this).append('<aside class="bookingInfo-end alert">Réservation impossible</aside>');
+				$('.alert').delay(3000).fadeOut();
+			}
+		}else if(startSelected && !endSelected){
+			var $cells = $('#calendar td'),
+			idx_1 = $cells.index($('.selected-booking')),
+			idx_2 = $cells.index($(this));
+			if((!$cells.slice(idx_1, idx_2 + 1).is('.booked:not(.start-booking)') || ($('.selected-booking').hasClass('end-booking') && !$cells.slice(idx_1, idx_2 + 1).is('.booked:not(.start-booking):not(.end-booking)') && $cells.slice(idx_1, idx_2 + 1).filter('.end-booking').length <= 1)) && $cells.slice(idx_1, idx_2 + 1).length > 1){
+				endSelected = true;
+				$cells.slice(idx_1+1, idx_2).addClass('booking-time');
+				$(this).addClass('selected-booking');
+				$(this).append('<aside class="bookingInfo-end">Fin du séjour</aside>');
+			}else{
+				$(this).append('<aside class="bookingInfo-end alert">Réservation impossible</aside>');
+				$('.alert').delay(3000).fadeOut();
+			}
+		}else{
+			$('aside.bookingInfo-start, aside.bookingInfo-end').remove();
+			$('.selected-booking').removeClass('selected-booking');
+			$('.booking-time').removeClass('booking-time');
+			startSelected = false;
+			endSelected = false;
+		}
+	});
+
+	/* Set n calendars visible & enable navigation */
+
+	var show = 3;
+
+	$('#calendar .ui-datepicker-group').hide();
+	$('#calendar .ui-datepicker-group').slice(0, show).show().addClass('visible');
+
+	$('#calendar nav .prev').attr('disabled', 'disabled');
+
+	$('#calendar nav .prev').click(function(event) {
+		if(!$('#calendar .ui-datepicker-group-first').hasClass('visible')){
+			$('#calendar nav .next').removeAttr('disabled', 'disabled');
+			$prevCalendar = $('#calendar .ui-datepicker-group.visible').first().prevAll('.ui-datepicker-group').slice(0, show);
+			$('#calendar .ui-datepicker-group.visible').fadeOut(100, function() {
+				$prevCalendar.fadeIn(100);
+			});;
+			$('#calendar .ui-datepicker-group.visible').removeClass('visible');
+			$prevCalendar.addClass('visible');
+
+			if($('.ui-datepicker-group-first').hasClass('visible'))
+				$('#calendar nav .prev').attr('disabled', 'disabled');
+		}
+	});
+
+	$('#calendar nav .next').click(function(event) {
+		if(!$('#calendar .ui-datepicker-group-last').hasClass('visible')){
+			$('#calendar nav .prev').removeAttr('disabled', 'disabled');
+			$nextCalendar = $('#calendar .ui-datepicker-group.visible').last().nextAll('.ui-datepicker-group').slice(0, show);
+			$('#calendar .ui-datepicker-group.visible').fadeOut(100, function() {
+				$nextCalendar.fadeIn(100);
+			});;
+			$('#calendar .ui-datepicker-group.visible').removeClass('visible');
+			$nextCalendar.addClass('visible');
+
+			if($('.ui-datepicker-group-last').hasClass('visible'))
+				$('#calendar nav .next').attr('disabled', 'disabled');
+		}
+	});
+
+});
